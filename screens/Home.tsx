28dc8cca44b9db1c16a4aa8b3acaed580f2f72b7
bennyhodl <ben@blockspaces.com>
2022-04-29 12:30:38 -0400

    Bungalow Beach

--- screens/Home.tsx
+++ screens/Home.tsx
@@ -1,16 +1,18 @@
-import React from 'react';
-import {Text, Box, HStack} from 'native-base';
+import React, {useState} from 'react';
+import {Text, Box, HStack, Button} from 'native-base';
 import {useNavigation} from '@react-navigation/native';
 import {NativeStackNavigationProp} from '@react-navigation/native-stack';
 import {BitcoinBayParamList} from './BitcoinBayNavParams';
 import WalletFooter from '../components/WalletFooter';
-// import MapView, {Marker} from 'react-native-maps';
+import MapView, {Marker} from 'react-native-maps';
 import {Dimensions} from 'react-native';
+import axios from "axios"
 
 type HomeScreenProp = NativeStackNavigationProp<BitcoinBayParamList, 'Home'>;
 
 const Home = () => {
   const navigation = useNavigation<HomeScreenProp>();
+ 
   return (
     <Box
       _dark={{bg: 'primary.dark'}}
@@ -19,7 +21,7 @@ const Home = () => {
       flex={1}
       flexDirection="column"
       justifyContent="space-between">
-      {/* <MapView
+      <MapView
         style={{width: '100%', height: Dimensions.get('window').height - 100}}
         region={{
           latitude: 27.964157,
@@ -36,7 +38,16 @@ const Home = () => {
           }}
           pinColor="#ff581a"
         />
-      </MapView> */}
+        <Marker
+          title="Bungalow Beach"
+          description="Accepts Bitcoin!"
+          coordinate={{
+            latitude: 27.476550,
+            longitude: -82.702700,
+          }}
+          pinColor="#ff581a"
+        />
+      </MapView>
       <HStack>
         <WalletFooter navigation={navigation} />
       </HStack>
