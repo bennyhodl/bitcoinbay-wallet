b2b85ecd35cf795f5927dc5b70e9c596fb83d00d
bennyhodl <ben@blockspaces.com>
2022-05-30 12:03:50 -0400

    Comments

--- screens/DrawerNavigation.tsx
+++ screens/DrawerNavigation.tsx
@@ -1,19 +1,19 @@
-import React, {useState} from 'react';
+import React from 'react';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import BitcoinBayNavigation from './Navigation';
 import Transactions from "./Transactions"
-import Deals from "./Deals"
-import Map from "./Map"
-
+// import Deals from "./Deals"
+// import Map from "./Map"
+// import CreateWallet from './onboarding/CreateWallet';
 
 const Drawer = () => {
-  const BitcoinBayDrawer = createDrawerNavigator();    
+  const BitcoinBayDrawer = createDrawerNavigator();  
   return (
     <BitcoinBayDrawer.Navigator>
-      <BitcoinBayDrawer.Screen options={{headerShown: false}} name="Wallet" component={BitcoinBayNavigation} />
-      <BitcoinBayDrawer.Screen name="Transactions" component={Transactions} />
-      {/* <BitcoinBayDrawer.Screen name="Deals" component={Deals} /> */}
-      {/* <BitcoinBayDrawer.Screen name="Map" component={Map} /> */}
+          <BitcoinBayDrawer.Screen options={{headerShown: false}} name="Wallet" component={BitcoinBayNavigation}/>
+          <BitcoinBayDrawer.Screen name="Transactions" component={Transactions} />
+          {/* <BitcoinBayDrawer.Screen name="Map" component={Map} /> 
+          <BitcoinBayDrawer.Screen name="Deals" component={Deals} />  */}
     </BitcoinBayDrawer.Navigator>
   );
 };
