be786465937d77e48239dc69055e76176a452eec
bennyhodl <ben@blockspaces.com>
2022-05-30 12:03:35 -0400

    Loading & paid icons

--- components/Loading.tsx
+++ components/Loading.tsx
@@ -1,13 +1,14 @@
 import React from "react"
-import {Center, Text} from "native-base"
+import {Center} from "native-base"
+import {ActivityIndicator} from "react-native"
 
 const Loading = () => {
     return (
         <Center 
-        _dark={{bg: 'primary.dark'}}
-        _light={{bg: 'primary.light'}}
+        // _dark={{bg: 'primary.dark'}}
+        // _light={{bg: 'primary.light'}}
         flex={1}>
-            <Text>Loading...</Text>
+            <ActivityIndicator size="large" color="#ff581a" />
         </Center>
     )
 }
--- components/Paid.tsx
+++ components/Paid.tsx
@@ -1,5 +1,7 @@
 import React from "react"
 import {Center, Text} from "native-base"
+import {FontAwesomeIcon} from '@fortawesome/react-native-fontawesome';
+import {faCircleCheck, faCircleExclamation} from '@fortawesome/free-solid-svg-icons';
 
 type PaidProps = {
     paid: boolean
@@ -12,9 +14,15 @@ const Paid = (props: PaidProps) => {
         _light={{bg: 'primary.light'}}
         flex={1}>
            {paid ? 
-                <Text>Paid!</Text>
+                <>
+                    <FontAwesomeIcon icon={faCircleCheck} size={100} color="#ff581a" />
+                    <Text fontSize={20} fontWeight="bold" pt={5}>Payment sent!</Text>
+                </>
                 :
-                <Text>Failed.</Text>
+                <>
+                    <FontAwesomeIcon icon={faCircleExclamation} size={100} color="#ff0000" />
+                    <Text fontSize={20} fontWeight="bold" pt={5}>Failed.</Text>
+                </>
             }
         </Center>
     )
