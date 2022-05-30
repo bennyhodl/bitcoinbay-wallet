e8d21b034d0d6377f9fc2dd5c886b92b74b85f04
bennyhodl <ben@blockspaces.com>
2022-05-30 15:55:54 -0400

    Async storage for logging in

--- stores/UserStore.ts
+++ stores/UserStore.ts
@@ -1,14 +1,36 @@
-import { action, observable } from 'mobx';
+import { action, makeObservable, observable } from 'mobx';
+import AsyncStorage from "@react-native-async-storage/async-storage"
 
 export default class UserStore {
+
+    constructor() {
+        makeObservable(this)
+    }
+
     @observable loggedIn: boolean = false
     @observable user: string | null = null
 
     @action
-    logIn = () => this.loggedIn = true
+    logIn = async () => {
+        await AsyncStorage.setItem('loggedIn', 'true')
+        this.loggedIn = true
+        console.log("Logging in mobx", this.loggedIn)
+    }
     @action
-    logOut = () => this.loggedIn = false
-    
+    logOut = async () => {
+        await AsyncStorage.setItem('loggedIn', 'false')
+        this.loggedIn = false
+        console.log("Logging out", this.loggedIn)
+    }
+
+    @action
+    checkLoggedIn = async () => {
+        const login = await AsyncStorage.getItem('loggedIn')
+        console.log("Checking log in", login)
+        if (login === 'true') return true
+        return false
+    }
+
     @action
     addUser = (name:string) => this.user = name
 }
